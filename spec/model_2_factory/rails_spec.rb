c93914b62d9a85c1bcbb71bab253b2e6cb600dd0
indrajit <eendroroy@gmail.com>
2017-04-22 01:31:32 +0600

    fix tests

--- spec/model_2_factory/rails_spec.rb
+++ spec/model_2_factory/rails_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe Model2Factory do
   it 'has a version number' do
-    expect(Model2Factory::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(Model2Factory::Rails::Version::STRING).not_to be nil
   end
 end
