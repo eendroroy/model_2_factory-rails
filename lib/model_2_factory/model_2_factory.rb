6cab4f0747e82331385e02f16f399c0a7e48b143
indrajit <eendroroy@gmail.com>
2017-04-21 16:16:46 +0600

    refactor

---
+++ lib/model_2_factory/model_2_factory.rb
@@ -0,0 +1,7 @@
+require 'model_2_factory/rails/version'
+
+module Model2Factory
+  def self.rails_test
+    'model_2_factory-rails'
+  end
+end
--- lib/model_2_factory/rails.rb
+++
@@ -1,7 +0,0 @@
-require "model_2_factory/rails/version"
-
-module Model2Factory
-  module Rails
-    # Your code goes here...
-  end
-end
