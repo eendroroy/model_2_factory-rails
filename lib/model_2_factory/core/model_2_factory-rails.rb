2eb4b229719d0a8b6917d2ff8163f9069c8bd155
indrajit <eendroroy@gmail.com>
2017-04-22 00:38:15 +0600

    update default values

--- lib/model_2_factory/core/model_2_factory-rails.rb
+++ lib/model_2_factory/core/model_2_factory-rails.rb
@@ -6,10 +6,10 @@ module Model2Factory
       numeric: 1,
       decimal: 123.456,
       float: 123.456,
-      string: 'MyString',
-      text: 'MyText',
-      datetime: Time.now.to_s,
-      timestamp: Time.now.getutc.to_i.to_s,
+      string: "'MyString'",
+      text: "'MyText'",
+      datetime: "'Time.now.to_s'",
+      timestamp: "'Time.now.getutc.to_i.to_s'",
       boolean: true
     }.freeze
   end
