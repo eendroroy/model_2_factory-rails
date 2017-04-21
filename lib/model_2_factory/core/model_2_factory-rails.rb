122f6ebaa1dc57c05ff082e5d6f36068cf7e16b8
indrajit <eendroroy@gmail.com>
2017-04-22 00:30:12 +0600

    print model columns

---
+++ lib/model_2_factory/core/model_2_factory-rails.rb
@@ -0,0 +1,16 @@
+module Model2Factory
+  module Rails
+    DEFAULTS = {
+      integer: 1,
+      bigint: 1,
+      numeric: 1,
+      decimal: 123.456,
+      float: 123.456,
+      string: 'MyString',
+      text: 'MyText',
+      datetime: Time.now.to_s,
+      timestamp: Time.now.getutc.to_i.to_s,
+      boolean: true
+    }.freeze
+  end
+end
--- lib/model_2_factory/rails/model_2_factory.rb
+++ lib/model_2_factory/rails/model_2_factory.rb
@@ -3,7 +3,30 @@ require 'model_2_factory/rails/version'
 module Model2Factory
   module Rails
     def self.rails_test
-      "model_2_factory => factory_gen <== #{ARGV}"
+      print_attributes(ARGV[0].to_s)
+    end
+
+    def self.print_attributes(name)
+      Object.const_get(name).columns.each do |col|
+        next if col == 'updated_at' || col == 'created_at'
+        puts "#{col.name}:#{col.type}"
+      end
+    rescue NameError
+      warn_not_found name
+    end
+
+    def self.warn_not_found(name)
+      puts "Model '#{name}' not found."
+      puts 'Available Models :'
+
+      Dir['app/models/**/*.rb'].map do |f|
+        puts "\t#{f.chomp('.rb')[f.rindex('/') + 1..f.length].camelize}"
+      end
+    end
+
+    class << self
+      private :print_attributes
+      private :warn_not_found
     end
   end
 end
